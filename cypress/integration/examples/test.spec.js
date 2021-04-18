e1c6f2ac4f9aa204d1b23623fd7a34b0afe254be
sbmagar <sbmagar.sbm99@gmail.com>
2021-04-18 14:05:06 +0545

    changed file

--- cypress/integration/examples/test.spec.js
+++ cypress/integration/examples/test.spec.js
@@ -12,16 +12,4 @@ describe('Test E-Commerce', () =>
     it('Navigate to the cart', () => {
         cy.get('#nav-cart').click()
             })
-
-    it('Navigate to the sign In Page', () => {
-    cy.get('#a-autoid-0-announce > .a-size-base-plus').click()
-                         })  
-
-    it('Enter User ID', () => {
-    cy.get('.a-padding-extra-large')
-        .find('#ap_email')
-        .type('tester12@yopmail.com')
-                                                                         })                      
-
-  
 })
